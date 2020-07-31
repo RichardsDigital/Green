fa949cdcbe17b8594f43605d19ac850bb4ad550f
Daniel Richards <Kordifier@Gmail.com>
2020-07-31 23:31:35 +0100

    Cleared up sass and added form functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import logo from '../images/green-logo.svg';
 
 const Login = () => {
+
+    const [displayText, setDisplayText] = useState("Getting started with Green.");
+    const email = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email.current.value !== "") {
+        setDisplayText(`Welcome back ${email.current.value}!`);
+        }
+        else {
+            return null;
+        }
+    }
+
     return (
-        <div className="loginContainer">
+        <div id="loginContainer">
             <div className="logoContainer">
-                <img src={logo}/>
+                <img src={logo} alt="logo"/>
             </div>
             <div className="infoBox">
                 <h1>Example login screen</h1>
-                <p>Getting started with Green.</p>
+                <p>{displayText}</p>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="emailContainer">
-                    <label id="emailLabel" for="username">Email Address</label>
-                    <input type="text" id="username" name="username" />
+                    <label id="emailLabel">Email Address</label>
+                    <input type="text" id="username" ref={email}/>
                     <div className="checkboxContainer">
                         <input id="checkbox" name="checkbox" type="checkbox"/>
-                        <label>Remember this device</label>
+                        <label id="checkboxLabel">Remember this device</label>
                     </div>
                 </div>
                 <div className="submitButton">
-                    <input type="submit" value="Sign in" />
+                    <input type="submit" value="Submit"/>
                 </div>
             </form>
         </div>
